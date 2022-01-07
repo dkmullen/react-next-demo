9035af2ceb40f5da0db44d83aab72f983c2542c4
Dennis Mullen <dkmullen@gmail.com>
2022-01-07 09:21:05 -0500

    Fix menu

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,20 +1,26 @@
 import { Menu } from 'semantic-ui-react';
-
-import classes from './main-header.module.css';
-
-// Needs to work with Link or useRouter
-const items = [
-  { key: 'home', active: true, name: 'Home', href: '/' },
-  { key: 'other-data', name: 'Other Data', href: '/other' },
-  { key: 'events', name: 'Upcoming Events', href: '/events' },
-];
-
-const MenuExampleProps = () => <Menu items={items} />;
+import { useRouter } from 'next/router';
 
 export default function MainHeader() {
+  const router = useRouter();
+
+  function handleItemClick(e, { name }) {
+    router.push(name);
+  }
+
   return (
-    <div>
-      <div>{MenuExampleProps()}</div>
-    </div>
+    <Menu>
+      <Menu.Item name="/" onClick={handleItemClick}>
+        Home
+      </Menu.Item>
+
+      <Menu.Item name="/other" onClick={handleItemClick}>
+        Other Data
+      </Menu.Item>
+
+      <Menu.Item name="/events" onClick={handleItemClick}>
+        Upcoming Events
+      </Menu.Item>
+    </Menu>
   );
 }
